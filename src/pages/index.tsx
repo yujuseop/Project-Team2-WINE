7fe77e9dcc6a8ed850109ecf5da645578a0b96c9
HyeyeonJung <jhy20306@gmail.com>
2025-02-05 13:33:27 +0900

    feat: Primary&Calcel Button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import React from "react";
+import PrimaryButton from "@/components/PrimaryButton";
+import CancelButton from "@/components/CancelButton";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       </Head>
       <div>
         <p>Pretendard 적용</p>
+        <PrimaryButton size="small">primary</PrimaryButton>
+        <CancelButton size="small">Cancel</CancelButton>
       </div>
     </>
   );
