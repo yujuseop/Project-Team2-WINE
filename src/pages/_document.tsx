f5b7fe49dadc8fa084cad4fb65c09608367f4eb5
이승환 <ls991217@naver.com>
2025-02-06 01:02:18 +0900

    docs: next.config.tsx 파일에서 외부 도메인의 이미지를 허용하도록 수정 + meta태그 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,8 +6,11 @@ export default function Document() {
       <Head>
         {/* 공통 메타 태그나 favicon 등은 여기서 설정 */}
         <meta name="description" content="와인 관련 사이트" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          href="/assets/images/logo_black.svg"
+          type="image/svg+xml"
+        />
       </Head>
       <body>
         <Main />
