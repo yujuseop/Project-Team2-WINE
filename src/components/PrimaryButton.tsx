c0aa39e11625d825e809deb87a07d55b5ecced89
HyeyeonJung <jhy20306@gmail.com>
2025-02-05 22:41:02 +0900

    feat: Landing page

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -6,19 +6,26 @@ type ButtonProps = {
   size?: "small" | "regular";
   disabled?: boolean;
   children: React.ReactNode;
+  className?: string; // className을 추가적으로 받을 수 있도록 설정
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const PrimaryButton: React.FC<ButtonProps> = ({
   size = "regular",
   disabled,
   children,
+  className, // 추가된 className
   ...props
 }) => {
   return (
     <button
-      className={classNames(styles.primaryButton, styles[size], {
-        [styles.disabled]: disabled,
-      })}
+      className={classNames(
+        styles.primaryButton,
+        styles[size],
+        className && className,
+        {
+          [styles.disabled]: disabled,
+        }
+      )}
       disabled={disabled}
       {...props}
     >
