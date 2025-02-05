b6c700dbd9afd9af4648639c33b9dab538ee80c0
HyeyeonJung <jhy20306@gmail.com>
2025-02-05 14:53:11 +0900

    feat: header

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cfb37e6ba590448a")
+/******/ 		__webpack_require__.h = () => ("94e6d0edbcb4c6d5")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ src/components/Container.tsx
@@ -0,0 +1,17 @@
+import styles from "./Container.module.css";
+import React, { ReactNode } from "react";
+
+export default function Container({
+  className = "",
+  page = false,
+  children,
+}: {
+  className?: string;
+  page?: boolean;
+  children: ReactNode;
+}) {
+  const classNames = `${styles.container} ${
+    page ? styles.page : ""
+  } ${className}`;
+  return <div className={classNames}>{children}</div>;
+}
