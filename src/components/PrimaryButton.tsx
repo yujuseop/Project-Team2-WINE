c25f6a31d40803db520c489cad64b7ed372fe1d7
HyeyeonJung <jhy20306@gmail.com>
2025-02-05 17:28:32 +0900

    chore: styled > module

--- src/components/PrimaryButton.tsx
+++ src/components/PrimaryButton.tsx
@@ -1,30 +1,30 @@
-import styled from "styled-components";
+import React from "react";
+import styles from "./PrimaryButton.module.css";
+import classNames from "classnames";
 
 type ButtonProps = {
   size?: "small" | "regular";
+  disabled?: boolean;
+  children: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const PrimaryButton: React.FC<ButtonProps> = ({
+  size = "regular",
+  disabled,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      className={classNames(styles.primaryButton, styles[size], {
+        [styles.disabled]: disabled,
+      })}
+      disabled={disabled}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 };
-
-const PrimaryButton = styled.button<ButtonProps>`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 16px auto;
-  padding: 16px 0;
-  width: 100%;
-  color: var(--white);
-  font-size: ${({ size }) => (size === "small" ? "14px" : "16px")};
-  font-weight: 500;
-  line-height: ${({ size }) => (size === "small" ? "24px" : "26px")};
-  border-radius: ${({ size }) => (size === "small" ? "12px" : "16px")};
-  box-sizing: border-box;
-  background-color: var(--purple-100);
-  cursor: pointer;
-  transition: all 0.1s ease;
-
-  &:disabled {
-    background-color: var(--gray-500);
-    color: var(--black);
-  }
-`;
 
 export default PrimaryButton;
