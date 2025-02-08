3320794472dc214837703c18169b1f211fb57f22
이승환 <ls991217@naver.com>
2025-02-08 16:10:36 +0900

    style: ReviewCardList 반응형 작업

--- src/pages/wines/components/ReviewCardList.tsx
+++ src/pages/wines/components/ReviewCardList.tsx
@@ -32,13 +32,15 @@ const ReviewCardList: React.FC<ReviewListProps> = ({ reviews }) => {
         reviews.map((review) => <ReviewCard key={review.id} review={review} />)
       ) : (
         <div className={styles.empty_message}>
-          <Image
-            src="/assets/images/no_review.svg"
-            alt="리뷰 없음"
-            width={136}
-            height={136}
-            priority
-          />
+          <div className={styles.empty_message_img}>
+            <Image
+              src="/assets/images/no_review.svg"
+              alt="리뷰 없음"
+              fill
+              style={{ objectFit: "contain" }}
+              priority
+            />
+          </div>
           <p>작성된 리뷰가 없어요</p>
         </div>
       )}
