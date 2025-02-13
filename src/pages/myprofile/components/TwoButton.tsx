96cc145d65b9416af04f33ec00239ed12a9c41ab
HyeyeonJung <jhy20306@gmail.com>
2025-02-13 15:06:24 +0900

    feat: 삭제하기 클릭시  twoButton modal showup

--- src/pages/myprofile/components/TwoButton.tsx
+++ src/pages/myprofile/components/TwoButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PrimaryButton from "@/components/PrimaryButton";
 import SecondaryButton from "@/components/SecondaryButton";
+import styles from "./TwoButton.module.css";
 
 interface TwoButtonProps {
   onCancel: () => void;
@@ -9,10 +10,18 @@ interface TwoButtonProps {
 
 export default function TwoButton({ onCancel, onConfirm }: TwoButtonProps) {
   return (
-    <div>
-      <p>정말 삭제하시겠습니까?</p>
-      <SecondaryButton onClick={onCancel}>취소하기</SecondaryButton>
-      <PrimaryButton onClick={onConfirm}>삭제하기</PrimaryButton>
+    <div className={styles.modal_overlay}>
+      <div className={styles.modal}>
+        <p className={styles.question}>정말 삭제하시겠습니까?</p>
+        <div className={styles.button_wrapper}>
+          <SecondaryButton className={styles.button} onClick={onCancel}>
+            취소하기
+          </SecondaryButton>
+          <PrimaryButton className={styles.button} onClick={onConfirm}>
+            삭제하기
+          </PrimaryButton>
+        </div>
+      </div>
     </div>
   );
 }
