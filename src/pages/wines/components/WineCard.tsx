1d577cbcf1f456921a86bb4e03e10f6845729e18
이승환 <ls991217@naver.com>
2025-02-18 03:13:38 +0900

    fix: 와인상세페이지에서 와인이미지 없을시 기본 이미지 처리

--- src/pages/wines/components/WineCard.tsx
+++ src/pages/wines/components/WineCard.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Image from "next/image";
 import styles from "./WineCard.module.css";
 
@@ -9,22 +10,31 @@ interface Wine {
 }
 
 const WineCard = ({ wine }: { wine?: Wine }) => {
+  // 상태를 컴포넌트 외부에서 선언하여 조건문 안에서 호출되지 않도록 합니다.
+  const [imgSrc, setImgSrc] = useState(
+    wine?.image || "/assets/icon/empty_img.png"
+  );
+
+  const handleImageError = () => {
+    setImgSrc("/assets/icon/empty_img.png"); // 이미지 로딩 실패 시 기본 이미지로 설정
+  };
+
+  // wine이 없다면 fallback을 렌더링
   if (!wine) {
     return <div className={styles.card}>데이터를 불러올 수 없습니다.</div>;
   }
-
-  const imageUrl = wine.image || "/assets/icon/empty_img.png";
 
   return (
     <div className={styles.card}>
       {/* 와인 이미지 */}
       <div className={styles.card_img}>
         <Image
-          src={imageUrl}
+          src={imgSrc} // 상태에서 이미지 경로 사용
           alt={wine.name}
           fill
           style={{ objectFit: "contain" }}
           priority
+          onError={handleImageError} // 이미지 오류 발생 시 처리
         />
       </div>
       {/* 와인 정보 */}
