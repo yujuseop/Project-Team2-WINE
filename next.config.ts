892f28a02d1e5dd8c62dc52a6fce8d10ccf5ac88
이승환 <ls991217@naver.com>
2025-02-18 15:39:56 +0900

    fix: next.Config.ts수정

--- next.config.ts
+++ next.config.ts
@@ -3,35 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "search.pstatic.net" },
-      { protocol: "https", hostname: "dbscthumb-phinf.pstatic.net" },
-      { protocol: "https", hostname: "upload.wikimedia.org" },
-      { protocol: "https", hostname: "newneek.co" },
-      { protocol: "https", hostname: "d2phebdq64jyfk.cloudfront.net" },
-      { protocol: "https", hostname: "img.khan.co.kr", pathname: "/**" },
-      { protocol: "https", hostname: "wine21.speedgabia.com", pathname: "/**" },
-      { protocol: "https", hostname: "i.namu.wiki", pathname: "/**" },
-      { protocol: "https", hostname: "mcgrocer.com", pathname: "/**" },
-      { protocol: "https", hostname: "static.ebs.co.kr", pathname: "/**" },
-      { protocol: "https", hostname: "cafe24.poxo.com", pathname: "/**" },
-      { protocol: "https", hostname: "via.placeholder.com", pathname: "/**" },
-      { protocol: "https", hostname: "cdn.imweb.me", pathname: "/**" },
-      { protocol: "https", hostname: "assets.wine.com", pathname: "/**" },
-      {
-        protocol: "https",
-        hostname: "cdn.sommeliertimes.com",
-        pathname: "/**",
-      },
-      { protocol: "http", hostname: "k.kakaocdn.net" },
-      { protocol: "https", hostname: "winefolly.com", pathname: "/**" },
-      { protocol: "https", hostname: "www.vivino.com", pathname: "/**" },
-      { protocol: "https", hostname: "images.vivino.com", pathname: "/**" },
-      { protocol: "https", hostname: "wine-searcher.com", pathname: "/**" },
-      { protocol: "https", hostname: "senior.chosun.com" },
-      { protocol: "https", hostname: "encrypted-tbn0.gstatic.com" },
-      { protocol: "https", hostname: "d1e2y5wc27crnp.cloudfront.net" },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
   async redirects() {
     return [
